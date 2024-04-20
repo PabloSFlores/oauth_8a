809c6745c155844aa5f4b7dc475d23b35df4db8f
PabloSFlores <i20213tn013@utez.edu.mx>
2024-04-19 22:02:07 -0600

    [UPDATE] Vista de perfil

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
-        <div>
-            <img :src="$auth.user.picture" />
-            <h2>{{ $auth.user.name }}</h2>
-            <p>{{ $auth.user.email }}</p>
-        </div>
-
+        <b-card title="Información del usuario" class="my-5 mx-5">
+            <b-row>
+                <b-col sm="2">
+                    <b-img v-bind="mainProps" rounded fluid :src="$auth.user.picture"></b-img>
+                </b-col>
+                <b-col sm="10">
+                    <b-card-body>
+                        <b-card-text>
+                            <h2>{{ $auth.user.name }}</h2>
+                            <p>{{ $auth.user.email }}</p>
+                        </b-card-text>
+                    </b-card-body>
+                </b-col>
+            </b-row>
+        </b-card>
         <div>
             <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            mainProps: {
+                width: 150,
+                height: 100,
+                class: 'my-2 mx-2'
+            }
+        }
+    }
+}
+</script>
